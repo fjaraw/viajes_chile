c84f5c75d81bc9480b48bbccb0977c0a6b67a6f5
Fernando Jara <fjaraw@hotmail.com>
2024-03-11 21:40:57 -0300

    Secciones 'Inicio' y 'Quienes Somos'. Mejoras en hoja de estilo y script.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//Inicializar carrusel de bootstramp
+const carousel = new bootstrap.Carousel('.carousel');
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Examen de módulo 2
+Desarrollado por Fernando Jara
+
+#### Tecnologías Aplicadas
+- Bootstrap 5.3
+- jQuery 3.6
+- Font Awesome 6.5
+- Google Fonts
+
+#### Módulos de Bootstrap
+- Carrousel
+- Navbar
+- Card
+- Tooltip
