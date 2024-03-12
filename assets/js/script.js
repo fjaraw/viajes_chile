80629ed1db9fa2a74a5cd73044c63fa61982b553
Fernando Jara <fjaraw@hotmail.com>
2024-03-11 23:22:31 -0300

    Mejoras

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ const carousel = new bootstrap.Carousel('.carousel');
 // tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+//alerta del boton Enviar
+function alerta() {
+    alert('Su mensaje ha sido enviado.');
+}
+$('#btnEnviar').on('click',alerta);
